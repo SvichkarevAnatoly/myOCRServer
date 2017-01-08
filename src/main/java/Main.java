41402c9108dec8ee0be26083402284027a78c814
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-08 16:13:31 +0300

    refactor create package for servlets

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlet.RequestServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
