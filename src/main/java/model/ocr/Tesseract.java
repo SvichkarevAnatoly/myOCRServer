70a7939fca1a7f04f0ac93fffadd75b53bd2b62d
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-14 12:18:40 +0300

    feature tesseract

---
+++ src/main/java/model/ocr/Tesseract.java
@@ -0,0 +1,38 @@
+package model.ocr;
+
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.lept;
+import org.bytedeco.javacpp.tesseract;
+
+public class Tesseract {
+
+    private final tesseract.TessBaseAPI api;
+
+    public Tesseract(String lang) {
+        api = new tesseract.TessBaseAPI();
+
+        final String tessdataFolder = getClass().getResource("/").getFile();
+        if (api.Init(tessdataFolder, lang) != 0) {
+            System.err.println("Could not initialize tesseract.");
+        }
+    }
+
+    public void release() {
+        api.deallocate();
+    }
+
+    public void setCharWhitelist(String whitelist) {
+        api.SetVariable("tessedit_char_whitelist", whitelist);
+    }
+
+    public String ocr(lept.PIX image) {
+        api.SetImage(image);
+
+        BytePointer outText = api.GetUTF8Text();
+        final String ocrText = outText.getString();
+
+        outText.deallocate();
+
+        return ocrText;
+    }
+}
