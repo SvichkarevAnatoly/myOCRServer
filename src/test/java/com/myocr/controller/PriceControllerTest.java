957af9fb2e0a133ceb3c0c957fffaea343d00107
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-02-25 17:29:34 +0300

    test price controller save method

---
+++ src/test/java/com/myocr/controller/PriceControllerTest.java
@@ -0,0 +1,125 @@
+package com.myocr.controller;
+
+import com.myocr.Application;
+import com.myocr.controller.json.RequestPriceBody;
+import com.myocr.entity.City;
+import com.myocr.entity.CityShop;
+import com.myocr.entity.Price;
+import com.myocr.entity.ReceiptItem;
+import com.myocr.entity.Shop;
+import com.myocr.repository.CityRepository;
+import com.myocr.repository.CityShopRepository;
+import com.myocr.repository.ReceiptItemRepository;
+import com.myocr.repository.ShopRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class PriceControllerTest {
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private ShopRepository shopRepository;
+
+    @Autowired
+    private CityShopRepository cityShopRepository;
+
+    @Autowired
+    private ReceiptItemRepository receiptItemRepository;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private City spb;
+    private Shop auchan;
+    private CityShop spbAuchan;
+    private ReceiptItem pizza;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        mappingJackson2HttpMessageConverter = Arrays.stream(converters)
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+
+        shopRepository.deleteAll();
+        cityRepository.deleteAll();
+        cityShopRepository.deleteAll();
+        receiptItemRepository.deleteAll();
+
+        spb = cityRepository.save(new City("Spb"));
+        auchan = new Shop("Auchan");
+        spbAuchan = CityShop.link(spb, auchan);
+        auchan = shopRepository.save(auchan);
+
+        pizza = receiptItemRepository.save(new ReceiptItem("Pizza"));
+    }
+
+    @Test
+    public void save() throws Exception {
+        final Price price = new Price("15.00", pizza, spbAuchan);
+        final String savedPriceJson = json(
+                new RequestPriceBody(spb.getName(), auchan.getName(),
+                        pizza.getName(), price.getValue()));
+
+        mockMvc.perform(post("/prices/save/")
+                .contentType(contentType).content(savedPriceJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.value", is(price.getValue())))
+                .andExpect(jsonPath("$.receiptItem.name",
+                        is(price.getReceiptItem().getName())))
+                .andExpect(jsonPath("$.cityShop.city.name",
+                        is(price.getCityShop().getCity().getName())))
+                .andExpect(jsonPath("$.cityShop.shop.name",
+                        is(price.getCityShop().getShop().getName())));
+    }
+
+    private String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
+}
