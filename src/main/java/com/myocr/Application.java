e78ee73ea7fe98cdc812568a6840537a74764882
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-02-26 17:34:18 +0300

    feature init tesseract

--- src/main/java/com/myocr/Application.java
+++ src/main/java/com/myocr/Application.java
@@ -1,29 +1,14 @@
 package com.myocr;
 
-import com.myocr.entity.City;
-import com.myocr.repository.CityRepository;
-import com.myocr.repository.ShopRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.myocr.util.TessdataUtil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.transaction.Transactional;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
-
-    @Autowired
-    private CityRepository cityRepository;
-
-    @Autowired
-    private ShopRepository shopRepository;
-
-    private List<String> cityNames = Arrays.asList("Spb", "Nsk");
-    private List<String> shopNames = Arrays.asList("Auchan", "Prisma", "Karusel", "Megas");
-    private City spb;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -32,22 +17,6 @@ public class Application implements CommandLineRunner {
     @Override
     @Transactional
     public void run(String... strings) throws Exception {
-        /*spb = cityRepository.save(new City(cityNames.get(0)));
-        final City nsk = cityRepository.save(new City(cityNames.get(1)));
-
-        final Shop auchan = new Shop(shopNames.get(0));
-        final Shop prisma = new Shop(shopNames.get(1));
-        final Shop karusel = new Shop(shopNames.get(2));
-        final Shop megas = new Shop(shopNames.get(3));
-
-        CityShop.link(spb, auchan);
-        CityShop.link(spb, prisma);
-        CityShop.link(spb, karusel);
-        CityShop.link(nsk, auchan);
-        CityShop.link(nsk, megas);
-
-        shopRepository.save(Arrays.asList(auchan, prisma, karusel, megas));
-
-        // cityRepository.deleteAll();*/
+        TessdataUtil.extractTessdata();
     }
 }
