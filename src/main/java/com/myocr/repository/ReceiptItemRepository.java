223d447bd7b099a41507c7cce0300adad5d92e04
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-02-26 22:41:46 +0300

    feature find receipt items by city and shop

--- src/main/java/com/myocr/repository/ReceiptItemRepository.java
+++ src/main/java/com/myocr/repository/ReceiptItemRepository.java
@@ -3,8 +3,12 @@ package com.myocr.repository;
 import com.myocr.entity.ReceiptItem;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReceiptItemRepository extends CrudRepository<ReceiptItem, Long> {
-    ReceiptItem findByName(String name);
+    List<ReceiptItem> findByName(String name);
 
     ReceiptItem findByNameAndCityShopReceiptItemsCityShopCityNameAndCityShopReceiptItemsCityShopShopName(String receiptItemName, String cityName, String shopName);
+
+    List<ReceiptItem> findByCityShopReceiptItemsCityShopCityNameAndCityShopReceiptItemsCityShopShopName(String cityName, String shopName);
 }
