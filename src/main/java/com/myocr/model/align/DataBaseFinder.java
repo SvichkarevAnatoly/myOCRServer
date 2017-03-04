074e95a8e8b3831c3060f7c179f434fa14ce697e
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-04 12:25:28 +0300

    feature data base finder

--- src/main/java/com/myocr/model/align/DataBaseFinder.java
+++ src/main/java/com/myocr/model/align/DataBaseFinder.java
@@ -1,39 +1,58 @@
 package com.myocr.model.align;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 public class DataBaseFinder {
+    private final static int MATCH_NUMBER = 3;
+
     private final List<String> receiptItems;
 
     public DataBaseFinder(List<String> receiptItems) {
         this.receiptItems = receiptItems;
     }
 
-    public String find(String ocrReceiptItem) {
+    public List<Match> find(String ocrReceiptItem) {
         final Aligner aligner = new SimpleAligner();
         return find(ocrReceiptItem, aligner);
     }
 
-    public String find(String ocrReceiptItem, Aligner aligner) {
-        int bestScore = Integer.MIN_VALUE;
-        String bestScoreProduct = "";
-        for (String product : receiptItems) {
-            int alignScore = aligner.align(ocrReceiptItem, product);
-            if (alignScore > bestScore) {
-                bestScore = alignScore;
-                bestScoreProduct = product;
+    public List<Match> find(String ocrReceiptItem, Aligner aligner) {
+        final TreeSet<Match> bestMatches = new TreeSet<>(
+                (m1, m2) -> Double.compare(m1.getScore(), m2.getScore() - 0.5d));
+
+        final int min = Math.min(MATCH_NUMBER, receiptItems.size());
+        for (int i = 0; i < min; i++) {
+            final String receiptItem = receiptItems.get(i);
+            int alignScore = aligner.align(ocrReceiptItem, receiptItem);
+            bestMatches.add(new Match(receiptItem, alignScore));
+        }
+
+        if (receiptItems.size() > MATCH_NUMBER) {
+            for (int i = 3; i < receiptItems.size(); i++) {
+                final String receiptItem = receiptItems.get(i);
+                int alignScore = aligner.align(ocrReceiptItem, receiptItem);
+
+                final Match smallest = bestMatches.first();
+                if (smallest.getScore() < alignScore) {
+                    bestMatches.pollFirst();
+                    bestMatches.add(new Match(receiptItem, alignScore));
+                }
             }
         }
 
-        return bestScoreProduct;
+        final List<Match> result = new ArrayList<>(bestMatches);
+        Collections.reverse(result);
+        return result;
     }
 
-    public List<String> findAll(List<String> ocrReceiptItems) {
-        final ArrayList<String> bestScoreProducts = new ArrayList<>(ocrReceiptItems.size());
+    public List<List<Match>> findAll(List<String> ocrReceiptItems) {
+        final List<List<Match>> bestScoreProducts = new ArrayList<>(ocrReceiptItems.size());
         for (String ocrProduct : ocrReceiptItems) {
-            final String bestScoreProduct = find(ocrProduct);
-            bestScoreProducts.add(bestScoreProduct);
+            final List<Match> matches = find(ocrProduct);
+            bestScoreProducts.add(matches);
         }
         return bestScoreProducts;
     }
