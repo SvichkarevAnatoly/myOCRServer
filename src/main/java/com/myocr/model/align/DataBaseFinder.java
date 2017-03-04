59a162051c67c910f1e87ccc1e1fd53d09918191
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-04 10:59:28 +0300

    refactor rename field

--- src/main/java/com/myocr/model/align/DataBaseFinder.java
+++ src/main/java/com/myocr/model/align/DataBaseFinder.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataBaseFinder {
-    private final List<String> products;
+    private final List<String> receiptItems;
 
-    public DataBaseFinder(List<String> products) {
-        this.products = products;
+    public DataBaseFinder(List<String> receiptItems) {
+        this.receiptItems = receiptItems;
     }
 
-    public String find(String ocrProduct) {
+    public String find(String ocrReceiptItem) {
         final Aligner aligner = new SimpleAligner();
-        return find(ocrProduct, aligner);
+        return find(ocrReceiptItem, aligner);
     }
 
-    public String find(String ocrProduct, Aligner aligner) {
+    public String find(String ocrReceiptItem, Aligner aligner) {
         int bestScore = Integer.MIN_VALUE;
         String bestScoreProduct = "";
-        for (String product : products) {
-            int alignScore = aligner.align(ocrProduct, product);
+        for (String product : receiptItems) {
+            int alignScore = aligner.align(ocrReceiptItem, product);
             if (alignScore > bestScore) {
                 bestScore = alignScore;
                 bestScoreProduct = product;
@@ -29,9 +29,9 @@ public class DataBaseFinder {
         return bestScoreProduct;
     }
 
-    public List<String> findAll(List<String> ocrProducts) {
-        final ArrayList<String> bestScoreProducts = new ArrayList<>(ocrProducts.size());
-        for (String ocrProduct : ocrProducts) {
+    public List<String> findAll(List<String> ocrReceiptItems) {
+        final ArrayList<String> bestScoreProducts = new ArrayList<>(ocrReceiptItems.size());
+        for (String ocrProduct : ocrReceiptItems) {
             final String bestScoreProduct = find(ocrProduct);
             bestScoreProducts.add(bestScoreProduct);
         }
