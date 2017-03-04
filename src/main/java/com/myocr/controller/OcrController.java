8efc5665ddac7bc9686a90ff28d85c9b8596bc4c
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-04 13:32:59 +0300

    feature find receiptItems for image

--- src/main/java/com/myocr/controller/OcrController.java
+++ src/main/java/com/myocr/controller/OcrController.java
@@ -1,7 +1,10 @@
 package com.myocr.controller;
 
+import com.myocr.model.align.ReceiptItemMatches;
 import com.myocr.model.ocr.OcrUtil;
 import com.myocr.model.ocr.Tesseract;
+import com.myocr.repository.ReceiptItemRepository;
+import com.myocr.service.ReceiptItemService;
 import org.bytedeco.javacpp.lept;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,8 +24,15 @@ import java.util.List;
 public class OcrController {
     final static Logger log = LoggerFactory.getLogger(OcrController.class);
 
+    private final ReceiptItemRepository receiptItemRepository;
+
+    public OcrController(ReceiptItemRepository receiptItemRepository) {
+        this.receiptItemRepository = receiptItemRepository;
+    }
+
     @PostMapping("/image")
-    public List<String> ocrImage(@RequestParam("file") MultipartFile image) throws IOException {
+    public List<ReceiptItemMatches> ocrImage(@RequestParam("file") MultipartFile image,
+                                             @RequestParam String city, @RequestParam String shop) throws IOException {
         log.info(image.getOriginalFilename());
 
         final lept.PIX pix = lept.pixReadMem(image.getBytes(), image.getSize());
@@ -34,7 +44,10 @@ public class OcrController {
 
         log.info(ocrText);
 
-        return OcrUtil.parse(ocrText);
+        final List<String> ocrReceiptItems = OcrUtil.parse(ocrText);
+
+        final ReceiptItemService service = new ReceiptItemService(receiptItemRepository);
+        return service.findReceipt(city, shop, ocrReceiptItems);
     }
 
     private File save(MultipartFile file) throws IOException {
