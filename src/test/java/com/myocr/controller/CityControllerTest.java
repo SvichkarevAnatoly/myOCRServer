1e85357ee6f44636a8eb9ef6f20a6e0abfd57f4b
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-04 20:14:37 +0300

    test get all cities controller

---
+++ src/test/java/com/myocr/controller/CityControllerTest.java
@@ -0,0 +1,68 @@
+package com.myocr.controller;
+
+import com.myocr.Application;
+import com.myocr.entity.City;
+import com.myocr.repository.CityRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+@TestPropertySource(locations = "classpath:test.properties")
+public class CityControllerTest {
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+
+    private List<String> cityNames = Arrays.asList("Spb", "Nsk");
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+
+        cityRepository.save(new City(cityNames.get(0)));
+        cityRepository.save(new City(cityNames.get(1)));
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        mockMvc.perform(get("/cities/all/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0]", is(cityNames.get(0))))
+                .andExpect(jsonPath("$[1]", is(cityNames.get(1))));
+    }
+}
