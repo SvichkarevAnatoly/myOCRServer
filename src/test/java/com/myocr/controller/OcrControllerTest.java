34c014c9979d8af172096230646362cf28cdf50d
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-05 12:07:08 +0300

    test template for ocr controller

---
+++ src/test/java/com/myocr/controller/OcrControllerTest.java
@@ -0,0 +1,76 @@
+package com.myocr.controller;
+
+import com.myocr.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+@TestPropertySource(locations = "classpath:test.properties")
+public class OcrControllerTest {
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+        mappingJackson2HttpMessageConverter = Arrays.stream(converters)
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void ocrImage() throws Exception {
+        final ClassLoader classLoader = getClass().getClassLoader();
+
+        final InputStream receiptItemsImageContent = classLoader.getResourceAsStream("images/products.jpg");
+        final InputStream pricesImageContent = classLoader.getResourceAsStream("images/prices.jpg");
+
+        final MockMultipartFile receiptItemsImage = new MockMultipartFile(
+                "receiptItemsImage", "products.jpg", null, receiptItemsImageContent);
+        final MockMultipartFile pricesImage = new MockMultipartFile(
+                "pricesImage", "prices.jpg", null, pricesImageContent);
+
+        mockMvc.perform(fileUpload("/ocr/image?city=Sbp&shop=Auchan")
+                .file(receiptItemsImage)
+                .file(pricesImage))
+                .andExpect(status().isOk());
+    }
+}
