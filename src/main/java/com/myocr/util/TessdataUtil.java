1c0335a7975c511f188f51c7cb53be9fe4600442
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-08 18:10:50 +0300

    fix get trained data by ClassPathResource

--- src/main/java/com/myocr/util/TessdataUtil.java
+++ src/main/java/com/myocr/util/TessdataUtil.java
@@ -1,5 +1,7 @@
 package com.myocr.util;
 
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +12,7 @@ import java.util.List;
 public class TessdataUtil {
     private static final String TESSFOLDER = "tessdata";
     private static final String TESSDATA_EXTENTION = ".traineddata";
-    private static final String JAR_ROOT = "/BOOT-INF/classes/";
+    private static final String JAR_ROOT = "/";
 
     private static final List<String> langs = Arrays.asList("eng", "rus");
 
@@ -19,18 +21,18 @@ public class TessdataUtil {
             final String fileName = lang + TESSDATA_EXTENTION;
             final String filePath = TESSFOLDER + File.separator + fileName;
             try {
-                extractFile(JAR_ROOT + filePath, filePath);
+                extractFile(filePath, filePath);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private static void extractFile(String resoursePath, String outPath) throws IOException {
+    private static void extractFile(String resourcePath, String outPath) throws IOException {
         File file = new File(outPath);
         if (!file.exists()) {
             file.getParentFile().mkdirs();
-            InputStream inputStream = file.getClass().getResourceAsStream(resoursePath);
+            InputStream inputStream = new ClassPathResource(resourcePath).getInputStream();
             Files.copy(inputStream, file.getAbsoluteFile().toPath());
         }
     }
