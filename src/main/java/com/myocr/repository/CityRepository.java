b25fac2c78cc608e0799d8e89dfec2bdb82162be
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-12 11:45:25 +0300

    feature find city by name repository method

--- src/main/java/com/myocr/repository/CityRepository.java
+++ src/main/java/com/myocr/repository/CityRepository.java
@@ -4,4 +4,5 @@ import com.myocr.entity.City;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CityRepository extends CrudRepository<City, Long> {
+    City findByName(String cityName);
 }
