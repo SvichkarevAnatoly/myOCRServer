7495f7efbd225e7fa90a4365f45bf83883b6a0c8
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-12 21:33:33 +0300

    refactor assert lower bound score of alignment

--- src/test/java/com/myocr/controller/OcrControllerTest.java
+++ src/test/java/com/myocr/controller/OcrControllerTest.java
@@ -1,6 +1,11 @@
 package com.myocr.controller;
 
+import com.google.gson.Gson;
 import com.myocr.Application;
+import com.myocr.controller.json.OcrReceiptResponse;
+import com.myocr.model.align.Aligner;
+import com.myocr.model.align.ReceiptItemMatches;
+import com.myocr.model.align.SimpleAligner;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,15 +19,20 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -73,25 +83,38 @@ public class OcrControllerTest {
         final MockMultipartFile pricesImage = new MockMultipartFile(
                 "pricesImage", "prices.jpg", null, pricesImageContent);
 
-        mockMvc.perform(fileUpload("/ocr/Sbp/Auchan")
-                .file(receiptItemsImage)
-                .file(pricesImage))
+        final MvcResult result = mockMvc
+                .perform(fileUpload("/ocr/Sbp/Auchan")
+                        .file(receiptItemsImage)
+                        .file(pricesImage))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(contentType))
 
                 .andExpect(jsonPath("$.itemMatches", hasSize(4)))
-                .andExpect(jsonPath("$.itemMatches[0].source",
-                        is("№№ Б/К0ЖИ КЧРИН0Е 0ХЛ. НА П0187. 14“ 187. И")))
-                .andExpect(jsonPath("$.itemMatches[1].source",
-                        is("№№ Б/К0ЖИ КЧРИНОЕ 0ХЛ. НА П0201. 00*1 201. 00")))
-                .andExpect(jsonPath("$.itemMatches[2].source",
-                        is("ИЗД—ИЕ МАКАР0Н. СЕРПАНТИН Г РЧПП1З. 90*1 13. 90")))
-                .andExpect(jsonPath("$.itemMatches[3].source",
-                        is("ХЛЕБ_ДАРНИШ<ИИ НАРЕЗКА ЗБ0Г 14. 90*1 14. 90")))
+                .andExpect(jsonPath("$.itemMatches[0].matches", empty()))
 
                 .andExpect(jsonPath("$.prices", hasSize(4)))
-                .andExpect(jsonPath("$.prices[0].stringValue", is("187.14")))
-                .andExpect(jsonPath("$.prices[0].intValue", is(18714)));
+                .andExpect(jsonPath("$.prices[0].intValue", is(18714)))
+                .andReturn();
+
+        final OcrReceiptResponse response = new Gson()
+                .fromJson(result.getResponse().getContentAsString(), OcrReceiptResponse.class);
+
+        assertOcrReceiptResponse(response);
+    }
+
+    private void assertOcrReceiptResponse(OcrReceiptResponse response) {
+        final String expectedItemSource = "ФИЛЕ Б/КОЖИ КУРИНОЕ ОХЛ. НА ПО187. 14*1 187. 14";
+
+        final List<ReceiptItemMatches> itemMatchesList = response.getItemMatches();
+        final ReceiptItemMatches itemMatches = itemMatchesList.get(0);
+        final String itemSource = itemMatches.getSource();
+
+        final Aligner aligner = new SimpleAligner();
+        final int score = aligner.align(itemSource, expectedItemSource);
+
+        final int expectedGreaterThanScore = expectedItemSource.length() / 3;
+        assertThat(score, greaterThan(expectedGreaterThanScore));
     }
 }
